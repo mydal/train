19e4e16f6f5e514802f5d7b8df43a0a08d16ee74
mydal <2049837924@qq.com>
2021-11-17 12:50:03 +0800

    su frist

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
-  <header>
+  <a-button type="primary">
+      Primary
+    </a-button>
+  <!-- <header>
     头部
-  </header>
-  <section class="app-container">
+  </header> -->
+  <!-- <section class="app-container">
     <menu>1231</menu>
     <section class="app-body">
       <router-view></router-view>
-    </section>
-  </section>
+    </section> -->
+  <!-- </section> -->
 </template>
 
 <style lang="less">
--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import { createApp } from 'vue'
+import { Button } from 'ant-design-vue'
 import App from './App.vue'
 import Route from '@/router'
 import router from './router'
 import store from './store'
 createApp(App).use(store).use(router)
     .use(Route)
+    .use(Button)
+
+    // .use(Button.name,Button)
+    // .component(Button.name, Button)
     .mount('#app')
